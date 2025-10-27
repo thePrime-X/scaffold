aea0e40ac3a4e271ee955388a1c1728faba14e91
thePrime-X <techprimex0@gmail.com>
2025-10-27 18:44:52 +0500

    Refactor hello.py by commenting out code and adding functions
    Comment out the add function and its usage, and add new functions toyou, add, and subtract.

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
 
-x = 1
-y = 2
+# x = 1
+# y = 2
 
-print(f"This is the sum: {x}, {y}, {add(x, y)}")
+# print(f"This is the sum: {x}, {y}, {add(x, y)}")
+
+def toyou(x):
+    return f"hi {x}"
+
+def add(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
